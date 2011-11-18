61d7f9796cb0e19c897ba98d52701b5a84c1e619
Alexander Bradner <abradner@dyn-129-78-250-202.wirelessguest.usyd.edu.au>
2011-11-18 11:08:52 +1100

    fixing monkey patch

--- config/initializers/patch_tenacity_table_missing.rb
+++ config/initializers/patch_tenacity_table_missing.rb
@@ -1,6 +1,6 @@
 #https://github.com/jwood/tenacity/issues/31
 module Tenacity::OrmExt::ActiveRecord::ClassMethods
-  def t_id_type
+  def _t_id_type
     @_t_id_type_clazz ||= begin
       Kernel.const_get(columns.find { |x| x.primary }.type.to_s.capitalize)
     rescue
