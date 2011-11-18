c866e8620a60e23e30276e18bcccf688181b1ac3
Shuqian Hon <shuqian@SQ-USyd.(none)>
2011-11-18 16:36:55 +1100

    attributes are posted to server. need to parse and store attribute mappings now.

--- app/models/import_mapping.rb
+++ app/models/import_mapping.rb
@@ -6,6 +6,8 @@ class ImportMapping
 
   field :mappings, :type => Hash
   field :name, :type => String
+  field :includes_header, :type => Boolean
+  field :delimiter, :type => String
 
   validates_uniqueness_of :name, :case_sensitive => false, :scope => [:data_object_id, :file_type_id], :message => "has been taken in this data object"
   validates_presence_of :data_object_id, :file_type_id, :name
