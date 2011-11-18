b96bc01f0f303f31292ac2a8a349d2cd7b6abd3f
Alexander Bradner <abradner@dyn-129-78-250-202.wirelessguest.usyd.edu.au>
2011-11-18 11:10:23 +1100

    experimenting

--- config/initializers/patch_tenacity_table_missing.rb
+++ config/initializers/patch_tenacity_table_missing.rb
@@ -1,5 +1,5 @@
 #https://github.com/jwood/tenacity/issues/31
-module Tenacity::OrmExt::ActiveRecord::ClassMethods
+Tenacity::OrmExt::ActiveRecord::ClassMethods.class_eval do
   def _t_id_type
     @_t_id_type_clazz ||= begin
       Kernel.const_get(columns.find { |x| x.primary }.type.to_s.capitalize)
